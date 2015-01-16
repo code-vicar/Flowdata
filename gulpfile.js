8516e30fa6649da5776f822adf58f68205bd31f0
Scott Vickers <scott.w.vickers@gmail.com>
2015-01-15 16:23:28 -0800

    only distribute single file in 'dist' folder, it will match the semver of the project, update to v0.1.6

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,12 @@
-var packageVersion = require('./package.json').version;
 var packageName = require('./package.json').name;
-var bundleName = packageName + '-' + packageVersion;
 
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-
-var getBundleName = function () {
-  return ;
-};
 
 // define tasks here
 gulp.task('dist', function () {
+  'use strict';
   var bundler = browserify({
     entries: ['./src/index.js'],
     standalone: 'flowdata',
@@ -22,9 +16,9 @@ gulp.task('dist', function () {
   var bundle = function() {
     return bundler
       .bundle()
-      .pipe(source(bundleName+'.js'))
+      .pipe(source(packageName+'.js'))
       .pipe(gulp.dest('./dist/'));
-  }
+  };
 
   return bundle();
 });
