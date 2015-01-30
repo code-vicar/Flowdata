d7b17c7879a9a3c45648f0bcaba1536775e0f574
Scott Vickers <scott.w.vickers@gmail.com>
2015-01-30 12:06:50 -0800

    add more test cases, extend mocha timeout, and update readme

--- README.md
+++ README.md
@@ -15,20 +15,18 @@ or if you're working in node
 ## Usage
 
 In the browser a global 'flowdata' object will be created
-
-    // * require for node only
+    
+    // ** require for node only
     var flowdata = require('flowdata');
-    // *
+    // **
+
+    var serviceUrl = 'http://services.odata.org/V4/(S(xloriihm0m3y04qekvvc3xg3))/TripPinServiceRW';
 
     // optionally set a logLevel
     flowdata.Utils.logging.logLevel = flowdata.Utils.logging.logLevels.debug;
 
     // initiate a new OData service
-    var service = new flowdata('http://services.odata.org/V4/(S(xloriihm0m3y04qekvvc3xg3))/TripPinServiceRW');
-
-    // a new service will have a 'ready' property which is a promise
-    //  that fulfills when the metadata has been retrieved and parsed
-    service.ready.then(function() {
+    flowdata.init(serviceUrl).then(function (service) {
       console.log(service.serviceUrl);
       console.dir(service.metadata);
     }).catch(function(err) {
--- test/tests.js
+++ test/tests.js
@@ -6,11 +6,53 @@
   var assert = require('assert');
 
   describe('Create a new service', function () {
-    it('it should fetch metadata', function (done) {
+    it('require a service url', function (done) {
+      flowdata.init().catch(function (err) {
+        assert.strictEqual(err, 'Must provide a serviceUrl');
+        done();
+      });
+    });
+
+    it('require a service url via options', function (done) {
+      flowdata.init({
+        metadata: {}
+      }).catch(function (err) {
+        assert.strictEqual(err, 'Must provide a serviceUrl');
+        done();
+      });
+    });
+
+    it('fetch metadata', function (done) {
       flowdata.init('http://services.odata.org/V4/(S(xloriihm0m3y04qekvvc3xg3))/TripPinServiceRW').then(function (service) {
         assert.notStrictEqual(service, null);
         assert.notStrictEqual(typeof service, 'undefined');
         assert.strictEqual(typeof service.metadata, 'object');
+        done();
+      });
+    });
+
+    it('fetch metadata via options', function (done) {
+      flowdata.init({
+        serviceUrl: 'http://services.odata.org/V4/(S(xloriihm0m3y04qekvvc3xg3))/TripPinServiceRW'
+      }).then(function (service) {
+        assert.notStrictEqual(service, null);
+        assert.notStrictEqual(typeof service, 'undefined');
+        assert.strictEqual(typeof service.metadata, 'object');
+        done();
+      });
+    });
+
+    it('accept pre-fetched metadata object', function (done) {
+      flowdata.init({
+        serviceUrl: 'http://services.odata.org/V4/(S(xloriihm0m3y04qekvvc3xg3))/TripPinServiceRW',
+        metadata: {
+          test: 'hi'
+        }
+      }).then(function (service) {
+        assert.notStrictEqual(service, null);
+        assert.notStrictEqual(typeof service, 'undefined');
+        assert.strictEqual(typeof service.metadata, 'object');
+        assert.strictEqual(service.metadata.test, 'hi');
         done();
       });
     });
