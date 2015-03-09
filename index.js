d69e78ed70a6b389c93b5c2361123840a4aaeee0
Scott Vickers <scott.w.vickers@gmail.com>
2015-03-09 13:16:56 -0700

    give up on root require, just move local_modules into node_modules as though it is a pre-installed package.  Update gitignore to allow local_modules through

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// patch the require module to allow loading relative to root with the '@' symbol
-var patch = require('./require-patch.js');
-patch.install(__dirname, {
-  verbose: true
-});
-
 var flowdata = require('./lib/flowdata');
 
 module.exports = flowdata;
